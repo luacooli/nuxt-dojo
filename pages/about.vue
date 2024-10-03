4651f3d328dda4825647c46e3ac66a67169e5512
Luana Correia <luacooli@gmail.com>
2024-10-03 11:27:53 -0300

    feat: visual structure w/ tailwind and layouts

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum veniam debitis dolore officiis nesciunt
+      ducimus id ullam voluptates magni inventore non commodi impedit sed dolores dolorem, consequatur at labore.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum veniam debitis dolore officiis nesciunt
+      ducimus id ullam voluptates magni inventore non commodi impedit sed dolores dolorem, consequatur at labore.
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, soluta
+      aspernatur quo, ab facilis excepturi eum eaque quas, suscipit totam ut et
+      molestiae consectetur corporis quis saepe quasi. Distinctio, possimus.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+
+definePageMeta({
+  layout: 'products',
+})
+</script>
+
+<style scoped></style>
